47149d976290168b7b831088fd26f6a8b9ad7d4b
Juan Diego <silgajuandiego@gmail.com>
2016-06-19 16:36:21 -0500

    añadido las cajas de dialogo para crear y editar los avisos en el LISTVIEW

--- app/src/main/java/com/inkodex/avisos/AvisosActivity.java
+++ app/src/main/java/com/inkodex/avisos/AvisosActivity.java
@@ -1,37 +1,58 @@
 package com.inkodex.avisos;
 
+import android.annotation.TargetApi;
 import android.app.Dialog;
 import android.database.Cursor;
+import android.os.Build;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.util.Log;
+import android.view.ActionMode;
+import android.view.MenuInflater;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.Window;
+import android.widget.AbsListView;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.EditText;
+import android.widget.LinearLayout;
 import android.widget.ListView;
+import android.widget.TextView;
 import android.widget.Toast;
+
+/**
+ * Created by JuanDiego on 18/06/2016.
+ */
 
 public class AvisosActivity extends AppCompatActivity {
     private ListView mListView;
     private AvisosDBAdapter mDbAdapter;
     private AvisosSimpleCursorAdapter mCursorAdapter;
 
+    @TargetApi(Build.VERSION_CODES.HONEYCOMB)
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_avisos);
+        ActionBar actionBar = getSupportActionBar();
+        actionBar.setHomeButtonEnabled(true);
+        actionBar.setDisplayShowHomeEnabled(true);
+        actionBar.setIcon(R.mipmap.ic_launcher);
+
         mListView = (ListView) findViewById(R.id.listView);
         findViewById(R.id.listView);
         mListView.setDivider(null);
         mDbAdapter = new AvisosDBAdapter(this);
         mDbAdapter.open();
-
 
 
         // cuando pulsamos un item individual en la  listview
@@ -40,7 +61,7 @@ public class AvisosActivity extends AppCompatActivity {
             public void onItemClick(AdapterView<?> parent, View view, final int masterListPosition, long id) {
                 AlertDialog.Builder builder = new AlertDialog.Builder(AvisosActivity.this);
                 ListView modeListView = new ListView(AvisosActivity.this);
-                String[] modes = new String[] { "Editar Aviso", "Borrar Aviso" };
+                String[] modes = new String[]{"Editar Aviso", "Borrar Aviso"};
                 ArrayAdapter<String> modeAdapter = new ArrayAdapter<>(AvisosActivity.this,
                         android.R.layout.simple_list_item_1, android.R.id.text1, modes);
                 modeListView.setAdapter(modeAdapter);
@@ -52,20 +73,19 @@ public class AvisosActivity extends AppCompatActivity {
                     public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
                         //editar aviso
                         if (position == 0) {
-                            Toast.makeText(AvisosActivity.this, "editar "
-                                    + masterListPosition, Toast.LENGTH_SHORT).show();
+                            int nId = getIdFromPosition(masterListPosition);
+                            Aviso aviso = mDbAdapter.fetchReminderById(nId);
+                            fireCustomDialog(aviso);
                             //borrar aviso
                         } else {
-                            Toast.makeText(AvisosActivity.this, "borrar "
-                                    + masterListPosition, Toast.LENGTH_SHORT).show();
+                            mDbAdapter.deleteReminderById(getIdFromPosition(masterListPosition));
+                            mCursorAdapter.changeCursor(mDbAdapter.fetchAllReminders());
                         }
                         dialog.dismiss();
                     }
                 });
             }
         });
-
-
 
 
         Cursor cursor = mDbAdapter.fetchAllReminders();
@@ -97,6 +117,103 @@ public class AvisosActivity extends AppCompatActivity {
         //el cursorAdapter (controller) está ahora actualizando la listView (view)
         //con datos desde la base de datos (modelo)
         mListView.setAdapter(mCursorAdapter);
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.HONEYCOMB) {
+
+            mListView.setChoiceMode(ListView.CHOICE_MODE_MULTIPLE_MODAL);
+            mListView.setMultiChoiceModeListener(new AbsListView.MultiChoiceModeListener() {
+
+                @Override
+                public void onItemCheckedStateChanged(ActionMode mode, int position, long id, boolean checked) {
+                }
+
+                @Override
+                public boolean onCreateActionMode(ActionMode mode, Menu menu) {
+                    MenuInflater inflater = mode.getMenuInflater();
+                    inflater.inflate(R.menu.cam_menu, menu);
+                    return true;
+                }
+
+                @Override
+                public boolean onPrepareActionMode(ActionMode mode, Menu menu) {
+                    return false;
+                }
+
+                @Override
+                public boolean onActionItemClicked(ActionMode mode, MenuItem item) {
+                    switch (item.getItemId()) {
+                        case R.id.menu_item_delete_aviso:
+                            for (int nC = mCursorAdapter.getCount() - 1; nC >= 0; nC--) {
+                                if (mListView.isItemChecked(nC)) {
+                                    mDbAdapter.deleteReminderById(getIdFromPosition(nC));
+                                }
+                            }
+                            mode.finish();
+                            mCursorAdapter.changeCursor(mDbAdapter.fetchAllReminders());
+                            return true;
+                    }
+                    return false;
+                }
+
+                @Override
+                public void onDestroyActionMode(ActionMode mode) {
+                }
+            });
+
+        }
+    }
+
+    private int getIdFromPosition(int nC) {
+        return (int) mCursorAdapter.getItemId(nC);
+    }
+
+    private void fireCustomDialog(final Aviso aviso) {
+        // custom dialog
+        final Dialog dialog = new Dialog(this);
+        dialog.requestWindowFeature(Window.FEATURE_NO_TITLE);
+        dialog.setContentView(R.layout.dialog_custom);
+
+        TextView titleView = (TextView) dialog.findViewById(R.id.custom_title);
+        final EditText editCustom = (EditText) dialog.findViewById(R.id.custom_edit_reminder);
+        Button commitButton = (Button) dialog.findViewById(R.id.custom_button_commit);
+        final CheckBox checkBox = (CheckBox) dialog.findViewById(R.id.custom_check_box);
+        LinearLayout rootLayout = (LinearLayout) dialog.findViewById(R.id.custom_root_layout);
+        final boolean isEditOperation = (aviso != null);
+
+        //esto es para un edit
+        if (isEditOperation) {
+            titleView.setText("Editar Aviso");
+            checkBox.setChecked(aviso.getImportant() == 1);
+            editCustom.setText(aviso.getContent());
+            rootLayout.setBackgroundColor(getResources().getColor(R.color.azul_neutro));
+        }
+
+        commitButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String reminderText = editCustom.getText().toString();
+                if (isEditOperation) {
+                    Aviso reminderEdited = new Aviso(aviso.getId(),
+                            reminderText, checkBox.isChecked() ? 1 : 0);
+                    mDbAdapter.updateReminder(reminderEdited);
+                    //esto es para nuevo aviso
+                } else {
+                    mDbAdapter.CreateReminder(reminderText, checkBox.isChecked());
+                }
+                mCursorAdapter.changeCursor(mDbAdapter.fetchAllReminders());
+                dialog.dismiss();
+            }
+        });
+
+        Button buttonCancel = (Button) dialog.findViewById(R.id.custom_button_cancel);
+        buttonCancel.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                dialog.dismiss();
+            }
+        });
+
+        dialog.show();
     }
 
 
@@ -111,8 +228,8 @@ public class AvisosActivity extends AppCompatActivity {
     public boolean onOptionsItemSelected(MenuItem item) {
         switch (item.getItemId()) {
             case R.id.action_nuevo:
-                //crear nuevo aviso
-                Log.d(getLocalClassName(), "crear  nuevo Aviso");
+                //crear nuevo Aviso
+                fireCustomDialog(null);
                 return true;
             case R.id.action_salir:
                 finish();
